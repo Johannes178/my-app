134c47d9fa42633071f243d39ad1799ee86ccf26
JJ <69968451+Johannes178@users.noreply.github.com>
2022-03-14 11:32:48 +0200

    harjotus1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <table>
+      <tbody>
+        <tr>
+          <td>
+            <img src="http://placekitten.com/162/160" alt="kiisu" />
+          </td>
+          <td>
+            <h3>Title</h3>
+            <p>Lorem ipsum dolor sit amet...</p>
+          </td>
+          <td>
+            <a href="https://metropolia.fi">View</a>
+          </td>
+        </tr>
+
+        <tr>
+          <td>
+            <img src="http://placekitten.com/163/160" alt="kiisu" />
+          </td>
+          <td>
+            <h3>Title</h3>
+            <p>Lorem ipsum dolor sit amet...</p>
+          </td>
+          <td>
+            <a href="https://metropolia.fi">View</a>
+          </td>
+        </tr>
+
+        <tr>
+          <td>
+            <img src="http://placekitten.com/160/160" alt="kiisu" />
+          </td>
+          <td>
+            <h3>Title</h3>
+            <p>Lorem ipsum dolor sit amet...</p>
+          </td>
+          <td>
+            <a href="https://metropolia.fi">View</a>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   );
-}
+};
 
 export default App;
+
+/*const mediaArray = [
+      {
+        'title': 'Title 1',
+        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales enim eget leo condimentum vulputate. Sed lacinia consectetur fermentum. Vestibulum lobortis purus id nisi mattis posuere. Praesent sagittis justo quis nibh ullamcorper, eget elementum lorem consectetur. Pellentesque eu consequat justo, eu sodales eros.',
+        'thumbnails': {
+          w160: 'http://placekitten.com/160/161',
+        },
+        'filename': 'http://placekitten.com/2048/1920',
+      },
+      {
+
+        'title': 'Title 2',
+        'description': 'Donec dignissim tincidunt nisl, non scelerisque massa pharetra ut. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Vestibulum tincidunt sapien eu ipsum tincidunt pulvinar. ',
+        'thumbnails': {
+          w160: 'http://placekitten.com/160/162',
+        },
+        'filename': 'http://placekitten.com/2041/1922',
+      },
+      {
+        'title': 'Title 3',
+        'description': 'Phasellus imperdiet nunc tincidunt molestie vestibulum. Donec dictum suscipit nibh. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. ',
+        'thumbnails': {
+          w160: 'http://placekitten.com/160/163',
+        },
+        'filename': 'http://placekitten.com/2039/1920',
+      },
+    ];*/
