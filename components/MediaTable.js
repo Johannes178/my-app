6a0f1f5bd8dbebb703990e5ca125e8714dc6dbd7
johannes <johannes.jokinen@metropolia.fi>
2022-03-25 13:47:50 +0200

    http-a

---
+++ components/MediaTable.js
@@ -0,0 +1,33 @@
+import {useState, useEffect} from 'react';
+import MediaRow from './MediaRow';
+
+
+const MediaTable = () => {
+  const [picArray, setPicArray] = useState([]);
+
+  useEffect(() => {
+    const loadMedia = async () => {
+      const response = await fetch('test.json');
+      const files = await response.json();
+      console.log(files);
+      setPicArray(files);
+    };
+
+    loadMedia();
+  }, []);
+
+  return (
+    <table>
+      <tbody>
+        {
+          picArray.map((item, index) => {
+            return <MediaRow key={index} file={item}/>;
+          })
+        }
+      </tbody>
+    </table>
+  );
+};
+
+
+export default MediaTable;
